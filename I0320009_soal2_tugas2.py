567f9af164e7382fb1400406de283ce357a7ba92
Angela R <angela.mutiaraputi@gmail.com>
2021-03-12 00:02:19 +0700

    Pengerjaan soal 2 tugas 2

---
+++ I0320009_soal2_tugas2.py
@@ -0,0 +1,35 @@
+#Memasukkan identitas pribadi
+namalengkap = "Angela Regine Mutiaraputi"
+namapanggilan = "Angela"
+nim = "I0320009"
+kelas = "A"
+prodi = "Teknik Industri"
+universitas = "Universitas Sebelas Maret"
+asalkota = "Bekasi"
+komplek = "Duta Bumi, Harapan Indah"
+hobi = "menari dan mendengarkan musik"
+print("Perkenalkan nama saya", str(namalengkap)+", biasa dipanggil", str(namapanggilan)+".")
+print("NIM saya", nim, "dari kelas",str(kelas)+".")
+print("Saya dari prodi", prodi, "di", str(universitas)+".")
+print("Saya berasal dari Kota", asalkota, "tepatnya di", str(komplek)+".")
+print("Saya memiliki hobi yaitu", str(hobi)+".")
+
+#Memasukkan tanggal lahir dan umur
+tempatlahir = "Jakarta"
+tanggallahir = int(6*4)
+bulanlahir = "Mei"
+tahunlahir = int(20*100+2)
+umur = int(36/2)
+print("Saya lahir di", tempatlahir, "pada tanggal", tanggallahir, bulanlahir, str(tahunlahir)+".")
+print("Saat ini saya berumur", umur, "tahun"+".")
+
+#Memasukkan data tambahan
+tinggibadan = int((50*2)+(126/2))
+beratbadan = float(111/2)
+ukuranbaju = "S atau M"
+ukurancelana = int(14*2)
+ukuransepatu = int(15*2+8)
+print("Tinggi badan saya adalah", tinggibadan, "cm.")
+print("Berat badan saya adalah", beratbadan, "kg.")
+print("Saya senang untuk menggunakan kaos dibanding kemeja dan saya memakai ukuran", str(ukuranbaju)+".")
+print("Saat berpergian saya senang memakai sneakers dan menggunakan ukuran", str(ukuransepatu)+".")
