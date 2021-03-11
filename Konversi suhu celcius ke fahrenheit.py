33e3bc5d86d90e9f31d3632465c2380dbf5e8d60
Angela R <angela.mutiaraputi@gmail.com>
2021-03-12 00:00:18 +0700

    Pengerjaan soal 2 tugas 2

---
+++ Konversi suhu celcius ke fahrenheit.py
@@ -0,0 +1,12 @@
+#menampilkan informasi program
+print("Konversi suhu Celcius ke Fahrenheit")
+
+#input suhu dalam celcius
+C = float(input("Masukkan suhu(celcius): "))
+
+#melakukan konversi suhu ke fahrenheit
+F = 9/5 * C + 32
+
+#menampilkan hasil konversi ke layar
+print("Celcius: ", C)
+print("Fahrenheit: ", F)
