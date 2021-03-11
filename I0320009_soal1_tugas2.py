b8c25dc7ee6d2625c393046229f5957cf5e7de0d
Angela R <angela.mutiaraputi@gmail.com>
2021-03-11 21:23:49 +0700

    Pengerjaan soal 1 tugas 2

---
+++ I0320009_soal1_tugas2.py
@@ -0,0 +1,76 @@
+import math
+
+#menampilkan informasi program
+print("Luas persegi panjang")
+
+#input panjang dan lebar
+p = float(input("Masukkan nilai panjang: "))
+l = float(input("Masukkan nilai lebar: "))
+
+#menghitung luas persegi panjang
+luas_persegipanjang = p * l
+
+#menampilkan hasil perhitungan ke layar
+print("luas persegi panjang: ", luas_persegipanjang)
+
+print("---------------")
+
+import math
+
+#menampilkan informasi program
+print("Luas lingkaran")
+
+#input nilai jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#menampilkan hasil perhitungan ke layar
+print("luas lingkaran: ", luas_lingkaran)
+
+print("---------------")
+
+import math
+
+#menampilkan informasi program
+print("Luas permukaan kubus")
+
+#input sisi kubus
+s = float(input("Masukkan sisi kubus: "))
+
+#menghitung luas permukaan kubus
+luas_permukaankubus = 6 * (s ** 2)
+
+#menampilkan hasil perhitungan ke layar
+print("luas permukaan kubus: ", luas_permukaankubus)
+
+print("---------------")
+
+#menampilkan informasi program
+print("Konversi suhu Celcius ke Fahrenheit")
+
+#input suhu dalam celcius
+C = float(input("Masukkan suhu(celcius): "))
+
+#melakukan konversi suhu ke fahrenheit
+F = 9/5 * C + 32
+
+#menampilkan hasil konversi ke layar
+print("Celcius: ", C)
+print("Fahrenheit: ", F)
+
+print("---------------")
+
+#menampilkan informasi program
+print("Konversi suhu reamur ke kelvin")
+
+#input suhu dalam reamur
+R = float(input("Masukkan suhu(kelvin): "))
+
+#melakukan konversi suhu reamur ke kelvin
+K = R * 5/4 + 273
+
+#menampilkan hasil konversi ke layar
+print("Reamur: ", R)
+print("Kelvin: ", K)
