36fec765f68ea303214ca31d8e28340b36e3607b
Angela R <angela.mutiaraputi@gmail.com>
2021-04-17 00:42:49 +0700

    Pengerjaan exercise

---
+++ Coba umur.py
@@ -0,0 +1,12 @@
+tanggalsekarang = 11
+bulansekarang = 3
+tahunsekarang = 2021
+tanggallahir = 24
+bulanlahir = 5
+tahunlahir = 2002
+
+tahunsekarang = int(tahunsekarang-tahunlahir)
+bulansekarang = int(bulanlahir-bulansekarang)
+tanggalsekarang = int(tanggallahir-tanggalsekarang)
+
+print("Umur saya adalah", tahunsekarang, "tahun", bulansekarang, "bulan", tanggalsekarang, "hari" )
